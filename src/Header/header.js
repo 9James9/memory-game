2f20e11005e32a1358c519a688d80355d238bf72
James Driscoll <thejamesdriscoll@gmail.com>
2022-02-24 13:13:33 -0600

    added design to header

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import pokeball from '../Images/pokeball.png'
+import './header.css'
 const Header = () => {
   return (
-    <div>Hello Header</div>
+    <div className='header__container'>
+      <h1 className='header__title'>Pokemon Memory Game</h1>
+      <img className="header__pokeball-image" src={pokeball} alt="pokeball"></img>
+    </div>
   )
 }
 
