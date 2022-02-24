e364b223db7dd454c04878a7a05bf8454817fa8e
James Driscoll <thejamesdriscoll@gmail.com>
2022-02-24 12:59:20 -0600

    added all pokemon images

--- src/Body/body.js
+++ src/Body/body.js
@@ -2,6 +2,17 @@ import React, {useState, useEffect} from 'react'
 import './body.css'
 import Card from '../Components/Card/card'
 import pikachu from '../Images/pikachu.jpg'
+import eevee from '../Images/eevee.jpg'
+import snorlax from '../Images/snorlax.jpg'
+import charizard from '../Images/charizard.jpg'
+import charmander from '../Images/charmander.jpg'
+import bulbasaur from '../Images/bulbasaur.jpg'
+import ditto from '../Images/ditto.jpg'
+import gardevoir from '../Images/gardevoir.jpg'
+import piplup from '../Images/piplup.jpg'
+import mewtwo from '../Images/mewtwo.jpg'
+import mew from '../Images/mew.jpg'
+import deoxys from '../Images/deoxys.jpg'
 const Body = () => {
     const [square, setSquare] = useState([])
     const [score, setScore] = useState(0)
@@ -24,24 +35,32 @@ const Body = () => {
             let names = {
                 0: 'Pikachu',
                 1: 'Eevee',
-                2: 'Snowlax',
+                2: 'Snorlax',
                 3: 'Charizard',
                 4: 'Charmander',
-                5: 'Garchomp',
+                5: 'Bulbasaur',
                 6: 'Ditto',
                 7: 'Gardevoir',
                 8: 'Piplup',
                 9: 'Mewtwo',
                 10: 'Mew',
                 11: 'Deoxys',
-                12: 'Squirtle'
             }
             let images = {
                 0: pikachu,
-                1: '../Images/eevee.jpg',
-                2: './'
+                1: eevee,
+                2: snorlax,
+                3: charizard,
+                4: charmander,
+                5: bulbasaur,
+                6: ditto,
+                7: gardevoir,
+                8: piplup,
+                9: mewtwo,
+                10: mew,
+                11: deoxys,
             }
-            let card = [i,names[i],images[0]]
+            let card = [i,names[i],images[i]]
             cards.push(card)
         }
         return cards
